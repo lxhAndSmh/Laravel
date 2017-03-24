e0615f257699983ff9e06e8130c1b9ae6b849f68
lxhAndSmh <Lxh_smh5558@163.com>
2017-03-24 19:08:11 +0800

    修改topic详情页的问题

--- app/src/main/java/com/liu/laravel/ui/topic/detail/TopicDetailActivity.java
+++ app/src/main/java/com/liu/laravel/ui/topic/detail/TopicDetailActivity.java
@@ -2,15 +2,27 @@ package com.liu.laravel.ui.topic.detail;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.liu.laravel.R;
+import com.liu.laravel.api.ApiHttpClient;
 import com.liu.laravel.bean.user.User;
+import com.liu.laravel.util.ToastUtils;
 import com.orhanobut.logger.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,8 +32,16 @@ public class TopicDetailActivity extends AppCompatActivity implements TopicDetai
     private TopicDetailContract.DataManager mManager;
     private TopicDetailContract.Presenter mPresenter;
 
-/*    @BindView(R.id.toolbar)
-    Toolbar toolbar;*/
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.user_info_rl)
+    RelativeLayout rlUserInfo;
+    @BindView(R.id.user_img_iv)
+    SimpleDraweeView dvAvatar;
+    @BindView(R.id.user_name_tv)
+    TextView tvName;
+    @BindView(R.id.user_desc_tv)
+    TextView tvDesc;
     @BindView(R.id.webView)
     WebView webView;
     @BindView(R.id.point_tv)
@@ -38,15 +58,42 @@ public class TopicDetailActivity extends AppCompatActivity implements TopicDetai
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic_detail);
         ButterKnife.bind(this);
+
+        initView();
         initData();
     }
 
-    private void initData(){
+    private void initView() {
+        setSupportActionBar(toolbar);
+        rlUserInfo.setVisibility(View.VISIBLE);
+    }
+
+    private void initData() {
         topicId = getIntent().getIntExtra("topicId", 0);
         mManager = new TopicDetailManager();
         mPresenter = new TopicDetailPresenter(mManager, this);
         mPresenter.getTopicDetailData(topicId);
         Logger.d("topicId====" + topicId);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toobar_menu_share, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_share:
+                ToastUtils.showShortTomast(this, "分享");
+                break;
+            default:
+                finish();
+                break;
+        }
+        return true;
     }
 
     @Override
@@ -59,12 +106,17 @@ public class TopicDetailActivity extends AppCompatActivity implements TopicDetai
     public void setWebUrl(String webUrl) {
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
-        webView.loadUrl(webUrl);
+        webView.loadUrl(webUrl, getAuth());
     }
 
     @Override
     public void setTitle(User userInfo) {
-
+        dvAvatar.setImageURI(userInfo.getAvatar());
+        tvName.setText(userInfo.getName());
+        if (!TextUtils.isEmpty(userInfo.getIntroduction())) {
+            tvDesc.setVisibility(View.VISIBLE);
+            tvDesc.setText(userInfo.getIntroduction());
+        }
     }
 
     @Override
@@ -96,4 +148,11 @@ public class TopicDetailActivity extends AppCompatActivity implements TopicDetai
     public void onRequestEnd() {
         progressBar.setVisibility(View.GONE);
     }
+
+    public Map<String, String> getAuth() {
+        Map<String, String> header = new HashMap<>();
+        header.put("Authorization", "Bearer " + ApiHttpClient.getmToken());
+        return header;
+    }
+
 }
